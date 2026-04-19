21658f025d0d2ab66104d34ce2a1f5705cca78b3
likhithakruthi <likhithakruthi35@gmail.com>
2026-04-19 04:10:19 +0000

    commit

--- battery_life.py
+++ battery_life.py
@@ -1,25 +1,20 @@
 def battery_life_predictor(capacity_mAh, I_active, I_sleep, T_active, T_sleep):
-    # Total time
+
     T_total = T_active + T_sleep
     
-    # Duty cycle
     D = T_active / T_total
     
-    # Average current
     I_avg = D * I_active + (1 - D) * I_sleep
     
-    # Battery life in hours
     battery_life_hours = capacity_mAh / I_avg
     
     return D, I_avg, battery_life_hours
 
-
-# Example values
-capacity = 2000       # mAh
-I_active = 100        # mA
-I_sleep = 5           # mA
-T_active = 2          # seconds
-T_sleep = 8           # seconds
+capacity = 2000
+I_active = 100
+I_sleep = 5
+T_active = 2
+T_sleep = 8           
 
 D, I_avg, life = battery_life_predictor(capacity, I_active, I_sleep, T_active, T_sleep)
 
